cb4c54f39661e6d3974637e85087da1ccecfa269
Earl Gay <earl@eeg3.net>
2016-10-21 15:32:52 -0400

    Hooked up steps to API.

--- ui/js/manifest.js
+++ ui/js/manifest.js
@@ -24,8 +24,8 @@ function initiateSteps(steps, substeps) {
     for (var j = 1; j<substeps+1; j++) {
 
       // Set checkbox names for current and next item
-      var checkboxA = "status-" + i + "-" + j;
-      var checkboxB = "status-" + i + "-" + (j*1 + 1);
+      var checkboxA = "status-substep-" + i + "-" + j;
+      var checkboxB = "status-substep-" + i + "-" + (j*1 + 1);
       var row = "row-substep-" + i + "-" + j;
 
       // White out everything except the first row at page open.
@@ -36,13 +36,14 @@ function initiateSteps(steps, substeps) {
       // Setting checkboxA's onclick to modify checkboxB's enabled/disabled status
       $("input#" + checkboxA).click(function(){
         checkboxA = this.id;
-        checkboxB = this.id.split("-")[0] + "-" + this.id.split("-")[1] + "-" + (this.id.split("-")[2]*1 + 1);
-        nextRow = "row-substep-" + checkboxB.split("-")[1] + "-" + checkboxB.split("-")[2];
+        checkboxB = this.id.split("-")[0] + "-" + this.id.split("-")[1] + "-" + this.id.split("-")[2] + "-" + (this.id.split("-")[3]*1 + 1);
+        nextRow = "row-substep-" + checkboxB.split("-")[2] + "-" + checkboxB.split("-")[3];
 
         // If checkboxB's length is 0 then time to modify next step's substep instead of a non-existing substep in this current step
+
         if (!($("#" + checkboxB).length)) {
-          checkboxB = this.id.split("-")[0] + "-" + (this.id.split("-")[1]*1 +1) + "-" + 1;
-          nextRow = "row-substep-" + checkboxB.split("-")[1] + "-1";
+          checkboxB = this.id.split("-")[0] + "-" + this.id.split("-")[1] + "-" + (this.id.split("-")[2]*1 +1) + "-" + 1;
+          nextRow = "row-substep-" + checkboxB.split("-")[2] + "-1";
         }
 
         // If checkboxA is checked, then enable the next checkbox; if it isn't, then disable the next checkbox
@@ -79,23 +80,55 @@ function parseSteps(steps) {
   var steps = steps.split(";");
   var numberOfSteps = steps.length;
 
-  // Insert code to create a quantity of shell tables based on numberOfSteps
 
   // Insert code to fill in the shell tables
   for (var i = 0;i < steps.length; i++) { // This for loop processes each step
     var stepNumber = i+1;
-    console.log("Step " + stepNumber);
+
+    var newDivHTML = '<div class="row"><div class="col-sm-12"><div class="row"><div class="col-md-12"><div class="panel"><div class="panel-heading sp-databox-panel-heading">Step ' + stepNumber + '</div><div id="' + stepNumber + '" class="panel-body sp-databox-panel-body"></div></div></div></div></div></div>';
+    $("#stepsSection").append(newDivHTML);
 
     var substeps = steps[i].split("|");
 
+
+    var table = $('<table></table>').addClass('table table-bordered table-striped sp-databox-table');;
+    var head = $('<thead><tr><td>Sub-Step</td><td>Details</td><td>Action</td><td>Notes&nbsp;<span data-toggle="tooltip" data-placement="top" title="Enter notes here to keep track of what you actually did."><i id="categoryInfo" class="fa fa-question-circle-o"></i></span></td><td>Status</td></tr></thead>');
+    var body = $('<tbody>');
+    table.append(head);
+    table.append(body);
+
     for (var j = 0; j < substeps.length; j++) { // This for loop processes each substep of each step
+      var substepNumber = j+1;
       var substepName = substeps[j].split(",")[0];
       var substepDetails = substeps[j].split(",")[1];
       var substepAction = substeps[j].split(",")[2];
+      var substepCode = "substep-" + stepNumber + "-" + substepNumber; // Example: substep-2-1
 
-      console.log("Sub-step # | Details | Action");
-      console.log(substepName + " | " + substepDetails + " | " + substepAction);
+      var tableLineItem = '<tr id="row-' + substepCode + '">';
+      tableLineItem += '<td id="' + substepCode + '" class="st-substep-col">' + substepName + '</td>';
+      tableLineItem += '<td id="details-' + substepCode + '" class="st-details-col">' + substepDetails + '</td>';
+      tableLineItem += '<td id="action-' + substepCode + '" class="st-action-col">' + substepAction + '</td>';
+      tableLineItem += '<td><input type="text" id="notes-' + substepCode + '" class="form-control input-sm" placeholder="Notes" /></td>';
+      if (i == 0 && j == 0) {
+        tableLineItem += '<td><input type="checkbox" id="status-' + substepCode + '"></td>';
+      } else {
+        tableLineItem += '<td><input type="checkbox" id="status-' + substepCode + '" disabled></td>';
+      }
+      tableLineItem += '</tr>';
+
+      table.append(tableLineItem);
+
+  //    console.log("Step Number: " + stepNumber);
+  //    console.log("Substep-Number: " + substepNumber);
+  //    console.log("Sub-step # | Details | Action");
+  //    console.log(substepName + " | " + substepDetails + " | " + substepAction);
+
     }
+
+    var tableEnd = $('</tbody>');
+    table.append(tableEnd);
+
+    $("#" + stepNumber).append(table);
 
   }
 
@@ -173,26 +206,25 @@ function getManifest() {
 
         var manifestSteps = manifestEntry.split("~")[8];
         parseSteps(manifestSteps);
-        //console.log("Steps: " + manifestSteps);
-        //$("#manifestSteps").html(manifestSteps);
 
       }
-      //$('#manifestDetailsTable').trigger("update");
+
+      // These should be calculated on the fly, but we'll set them statically for now
+      var steps=2;
+      var substeps=3;
+      initiateSteps(steps, substeps);
+
+      // Enable tooltips after all the steps are processed.
+      $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+      });
     });
+
 }
 
 function init () {
   currentTimestamp();
-
-  // These should be calculated on the fly, but we'll set them statically for now
-  var steps=2;
-  var substeps=3;
-
-  initiateSteps(steps, substeps);
-  $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-  $("#generateReport").click(function(){ window.print(); }); // Hook clicking Generate Completion Report button into printing the page
   getManifest();
-  //parseSteps();
+  $("#generateReport").click(function(){ window.print(); }); // Hook clicking Generate Completion Report button into printing the page
+
 }
